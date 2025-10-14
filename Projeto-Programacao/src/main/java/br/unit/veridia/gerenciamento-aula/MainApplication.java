b4e9685ce8b490682f4764efe15bf1b9e9370f28
Raul Souza <168578380+RaulRSouza@users.noreply.github.com>
2025-10-13 21:09:52 -0300

    Add files via upload

---
+++ Projeto-Programacao/src/main/java/br/unit/veridia/gerenciamento-aula/MainApplication.java
@@ -0,0 +1,13 @@
+package br.gov.se.pm.api_jm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJuntaMedicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJuntaMedicaApplication.class, args);
+	}
+
+}
---
+++ Projeto-Programacao/src/test/java/br/gov/se/pm/api_jm/ApiJuntaMedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.se.pm.api_jm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJuntaMedicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
